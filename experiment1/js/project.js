e7de6a72889181c3871a0f63e4bf48b09f4eb300
Nanot101 <luangiang001@gmail.com>
2025-04-05 15:34:28 -0700

    initial commit

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -23,12 +23,69 @@ class MyProjectClass {
 }
 
 function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+  // // create an instance of the class
+  // let myInstance = new MyProjectClass("value1", "value2");
 
-  // call a method on the instance
-  myInstance.myMethod();
+  // // call a method on the instance
+  // myInstance.myMethod();
+
+  const fillers = {
+    // adventurer: ["My dude", "Bro", "WesBot", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "勇者", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
+    // pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
+    // post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
+    // people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
+    // item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
+    // num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
+    // looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
+    // loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
+    // baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
+    // message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
+    male_baby: ["Jeff"],
+    female_baby: ["Monica"],
+    unisex_baby: ["Alex"],
+     
+  };
+  
+  const template = `
+  If your baby is a boy or has a genital of the male sex, I suggest you name it $male_baby.
+  
+  If your baby is a girl or has a genital of the female sex, I suggest you name it $female_baby.
+  
+  If you want a name that could be either considered as male or female, I suggest you name it $unisex_baby.
+  `;
+  
+  
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
+  }
+  
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    // box.innerText = story;
+    $("#box").text(story);
+  }
+  
+  /* global clicker */
+  // clicker.onclick = generate;
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
