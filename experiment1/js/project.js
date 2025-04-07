86f318e352ec223d073bb50166276f9428e23e85
Nanot101 <luangiang001@gmail.com>
2025-04-07 13:51:40 -0700

    more adjustments
    changed color of text, added more names,
    adjusted style

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -30,27 +30,15 @@ function main() {
   // myInstance.myMethod();
 
   const fillers = {
-    // adventurer: ["My dude", "Bro", "WesBot", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "勇者", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    // pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    // post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    // people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca", "帥氣"],
-    // item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    // num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    // looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    // loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    // baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    // message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
-    male_baby: ["Jeff"],
-    female_baby: ["Monica"],
-    unisex_baby: ["Alex"],
+    male_baby: ["Jeff", "Joe", "Josh", "Kyle", "John"],
+    female_baby: ["Monica","Monique", "Kiara", "Sarah"],
+    unisex_baby: ["Alex", "Abe", "Charlie", "Sam", "Taylor"],
      
   };
   
   const template = `
   If your baby is a boy or has a genital of the male sex, I suggest you name it $male_baby.
-  
   If your baby is a girl or has a genital of the female sex, I suggest you name it $female_baby.
-  
   If you want a name that could be either considered as male or female, I suggest you name it $unisex_baby.
   `;
   
@@ -59,14 +47,30 @@ function main() {
   
   const slotPattern = /\$(\w+)/;
   
+  // function replacer(match, name) {
+  //   let options = fillers[name];
+  //   if (options) {
+  //     return options[Math.floor(Math.random() * options.length)];
+  //   } else {
+  //     return `<UNKNOWN:${name}>`;
+  //   }
+  // }
   function replacer(match, name) {
     let options = fillers[name];
     if (options) {
-      return options[Math.floor(Math.random() * options.length)];
+      const colorMap = {
+        male_baby: "blue",
+        female_baby: "hotpink",
+        unisex_baby: "green",
+      };
+      const color = colorMap[name] || "black";
+      const chosen = options[Math.floor(Math.random() * options.length)];
+      return `<span style="color: ${color};">${chosen}</span>`;
     } else {
       return `<UNKNOWN:${name}>`;
     }
   }
+  
   
   function generate() {
     let story = template;
@@ -76,7 +80,8 @@ function main() {
   
     /* global box */
     // box.innerText = story;
-    $("#box").text(story);
+    // $("#box").text(story);
+    $("#box").html(story.replace(/\n/g, "<br>"));
   }
   
   /* global clicker */
